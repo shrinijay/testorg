869251d41e1a794820979561d5901936a65309db
shrinijay <srvs1994@gmail.com>
2025-04-29 14:12:30 +0530

    commiting

--- restapilearning/src/test/java/jiraincidents/GetCreateIssueMetadata.java
+++ restapilearning/src/test/java/jiraincidents/GetCreateIssueMetadata.java
@@ -10,10 +10,10 @@ public class GetCreateIssueMetadata {
 		
 
 
-		given().auth().preemptive().basic("srvs1994@gmail.com", "ATATT3xFfGF0H5pcxqqc0B3ZZWP85qaASX2mYBzXa-Dju5dhJZJ9-AyWTPcjyMc29Rax30hWDV8Rn2MV_dg756CF6hpU4I-6VnzP3znsV8CCTlYjhNu5sxH7TqzC8VwachjNv2JQhREcFxZ1DnEIGeTgAGJPRKtNHGTPbG7hURzMiPD-Q8bB7y8=57146E7C").header("Accept","application/json").log().all().when().get("rest/api/3/issue/createmeta").then().log().all();
+		given().auth().preemptive().basic("srvs1994@gmail.com", "").header("Accept","application/json").log().all().when().get("rest/api/3/issue/createmeta").then().log().all();
 		
 		//create an issue
-		given().auth().basic("srvs1994@gmail.com", "ATATT3xFfGF0H5pcxqqc0B3ZZWP85qaASX2mYBzXa-Dju5dhJZJ9-AyWTPcjyMc29Rax30hWDV8Rn2MV_dg756CF6hpU4I-6VnzP3znsV8CCTlYjhNu5sxH7TqzC8VwachjNv2JQhREcFxZ1DnEIGeTgAGJPRKtNHGTPbG7hURzMiPD-Q8bB7y8=57146E7C").header("Accept", "application/json").header("Content-Type", "application/json").body("{\r\n"
+		given().auth().basic("srvs1994@gmail.com", "").header("Accept", "application/json").header("Content-Type", "application/json").body("{\r\n"
 				+ "\"fields\":{\n"
 				+ "  \"summary\": \"New issue from Rest Assured Code\",\n"
 				+ "  \"issuetype\": {\n"
